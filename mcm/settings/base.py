558f04b90f62ae0ecb75c07c2ec3533c656cc36d
LuisParada7 <luisdanielparadamesa@gmail.com>
2026-04-08 14:10:32 -0500

    prepare application for deployment

--- mcm/settings/base.py
+++ mcm/settings/base.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 import environ
 
@@ -27,17 +27,11 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9!@graxa-t5@+hecr*&7cs8#*h8!_g%ffdok4rzvuivft#xz2^'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -59,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,12 +87,29 @@ WSGI_APPLICATION = 'mcm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+"""
+
+DATABASES = {
+
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
+    }
+}
+
 
 
 # Password validation
---
+++ mcm/settings/dev.py
@@ -0,0 +1,5 @@
+from .base import *
+
+SECRET_KEY = 'django-insecure-9!@graxa-t5@+hecr*&7cs8#*h8!_g%ffdok4rzvuivft#xz2^'
+DEBUG = True
+ALLOWED_HOSTS = []
